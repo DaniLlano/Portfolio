d4aaf6d63f493efc6e55668b685047b901b1123d
Dani <danillano97@gmail.com>
2021-06-21 01:30:19 -0300

    cambios

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -7,8 +7,8 @@ function Social() {
         <SocialContainer>
         <SocialTitle>Mis Redes</SocialTitle>
         <IconContainer>
-            <LinkedIn style={{ fontSize: 50 }} />
-            <Github style={{ fontSize: 40 }} />
+        <a href="https://www.linkedin.com/in/daniela-llano/"><LinkedIn style={{ fontSize: 50 }} /></a>
+        <a href="https://github.com/DaniLlano/"><Github style={{ fontSize: 40 }} /></a>
         </IconContainer>
         
             
