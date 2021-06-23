1691bc25bb31334cc664d2526c45d1ca9d077854
Daniela <danillano97@gmail.com>
2021-06-22 21:49:10 -0300

    cambios form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { FormContainer, ContactForm, ContactInput, ContactSubmit, TextInput, FormTitle } from './FormStyles'
 
 function Form() {
+
     return (
         <FormContainer>
             <FormTitle>Contactame</FormTitle>
-            <ContactForm action="https://getform.io/f/6beb522b-c5f3-4b9e-a245-a38491539538">
+            <ContactForm action="https://formsubmit.co/2414f39d3cb0a009504e484b14e1bb24" method="POST">
                 <ContactInput type="text" name="name" placeholder="Nombre" required/>
                 <ContactInput type="email" name="email" placeholder="Mail" required/>
                 <TextInput type="text" name="message" placeholder="Tu mensaje" required/>
+                <input type="hidden" name="_captcha" value="false"></input>
                 <ContactSubmit type="submit">Enviar</ContactSubmit>
             </ContactForm>
         </FormContainer>
