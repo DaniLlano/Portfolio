5c18adf938a018b07776e74e855dd12a7c0437d3
Daniela <danillano97@gmail.com>
2021-06-22 21:52:40 -0300

    cambios form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -6,7 +6,7 @@ function Form() {
     return (
         <FormContainer>
             <FormTitle>Contactame</FormTitle>
-            <ContactForm action="https://formsubmit.co/2414f39d3cb0a009504e484b14e1bb24" method="POST">
+            <ContactForm action="https://formsubmit.co/2414f39d3cb0a009504e484b14e1bb2" method="POST">
                 <ContactInput type="text" name="name" placeholder="Nombre" required/>
                 <ContactInput type="email" name="email" placeholder="Mail" required/>
                 <TextInput type="text" name="message" placeholder="Tu mensaje" required/>
